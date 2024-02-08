0a06885bea42dacea18073fd8ccdd626f514345c
christian <christian.doriath@free.Fr>
2024-02-08 10:11:11 +0100

    petit

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def my_git_update():
     # >>> <class 'git.remote.Remote'>  
 
     print("origin : ",origin)
-    print("*************************************************")
+    print("**************************************************")
     repo.create_head('main',origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
     origin.pull()
     print("'origin.pull()' a été fait ...")
